f66f194433b33ecaecbf3266a22e14ef09e441f1
arul1234 <arulrajamca@gmail.com>
2019-04-23 16:36:23 +0530

    Google Ads

---
+++ MainActivity.java
@@ -0,0 +1,75 @@
+package com.girisir;
+
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+public class MainActivity extends AppCompatActivity {
+    WebView webView;
+    SwipeRefreshLayout swipe;
+    private static final String TAG = "MainActivity";
+
+    private AdView mAdView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+        swipe = (SwipeRefreshLayout) findViewById(R.id.swipe);
+        swipe.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+
+                LoadWeb();
+            }
+        });
+
+        LoadWeb();
+    }
+
+    public void LoadWeb(){
+
+        webView = (WebView) findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setAppCacheEnabled(true);
+        webView.loadUrl("https://girisirapp.blogspot.com/p/blog-page.html");
+        swipe.setRefreshing(true);
+        webView.setWebViewClient(new WebViewClient(){
+
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+
+                webView.loadUrl("file:///android_asset/error.html");
+            }
+
+            public  void  onPageFinished(WebView view, String url){
+
+                //Hide the SwipeReefreshLayout
+
+                swipe.setRefreshing(false);
+            }
+
+        });
+    }
+
+
+    @Override
+    public void onBackPressed(){
+
+        if (webView.canGoBack()){
+            webView.goBack();
+        }else {
+            finish();
+        }
+    }
+
+
+
+}
